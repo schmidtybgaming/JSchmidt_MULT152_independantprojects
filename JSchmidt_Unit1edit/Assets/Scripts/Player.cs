6d8c07b3096d88b12b5b35bc91234a6712518505
schmidtybgaming <schmidtybballin@gmail.com>
2024-08-31 15:19:07 -0700

    update

--- JSchmidt_Unit1edit/Assets/Scripts/Player.cs
+++ JSchmidt_Unit1edit/Assets/Scripts/Player.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float speed = 15.0f;
+    private float speed = 15.0f;
+    private float turnSpeed = 15.0f;
+
+    private float horizontalInput;
+    private float verticalInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +18,10 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
         Debug.Log(Time.deltaTime);
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }

bc5c8ac7d26cc8a120a22d2e671420fd4367bfdb
schmidtybgaming <schmidtybballin@gmail.com>
2024-09-12 16:17:47 -0700

    update

--- JSchmidt_Unit2/Assets/Course Library/Scripts/Dissapear.cs
+++ JSchmidt_Unit2/Assets/Course Library/Scripts/Dissapear.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Dissapear : MonoBehaviour
 {
+    private float topOfScene = 30.0f;
+    private float bottomOfScene = -10.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,14 @@ public class Dissapear : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(transform.position.z > topOfScene)
+        {
+            Destroy(gameObject);
+        }
+
+        else if(transform.position.z < bottomOfScene)
+        {
+            Destroy(gameObject);
+        }
     }
 }
